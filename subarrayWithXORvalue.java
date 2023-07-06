72736e82ea225b678dfc6e516e12cadb29b3cb62
devesh-d16 <77147378+devesh-d16@users.noreply.github.com>
2023-07-06 23:29:01 +0530

    hasmap bhaiya fuml

---
+++ subarrayWithXORvalue.java
@@ -0,0 +1,33 @@
+// Given an array of integers A and an integer B.
+// Find the total number of subarrays having bitwise XOR of all elements equals to B.
+
+public int solve(ArrayList<Integer> a, int K) {
+        int n = a.size();
+        int xr = 0;
+        Map<Integer, Integer> mpp = new HashMap<>(); //declaring the map.
+        mpp.put(xr, 1); //setting the value of 0.
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            // prefix XOR till index i:
+            xr = xr ^ a.get(i);
+
+            //By formula: x = xr^k:
+            int x = xr ^ K;
+
+            // add the occurrence of xr^k
+            // to the count:
+            if (mpp.containsKey(x)) {
+                cnt += mpp.get(x);
+            }
+
+            // Insert the prefix xor till index i
+            // into the map:
+            if (mpp.containsKey(xr)) {
+                mpp.put(xr, mpp.get(xr) + 1);
+            } else {
+                mpp.put(xr, 1);
+            }
+        }
+        return cnt;
+    }
