0f684fa397e8e63cc45cee12b6252fb1123967dc
devesh-d16 <77147378+devesh-d16@users.noreply.github.com>
2023-07-07 18:28:02 +0530

    tough hai bhai

---
+++ CountInversions.java
@@ -0,0 +1,69 @@
+// TC - O(NlogN)
+// SC - O(N)
+// Merge Sort Algo
+
+// Given an array of integers. Find the Inversion Count in the array. 
+// Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. 
+// If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the maximum. 
+// Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+
+// Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+// Output: 3
+// Explanation: The sequence 2, 4, 1, 3, 5 
+// has three inversions (2, 1), (4, 1), (4, 3).
+
+    private static int merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+        int left = low;      // starting index of left half of arr
+        int right = mid + 1;   // starting index of right half of arr
+
+        //Modification 1: cnt variable to count the pairs:
+        int cnt = 0;
+
+        //storing elements in the temporary array in a sorted manner//
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                cnt += (mid - left + 1); //Modification 2
+                right++;
+            }
+        }
+
+        // if elements on the left half are still left //
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        //  if elements on the right half are still left //
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        // transfering all elements from temporary to arr //
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+        return cnt; // Modification 3
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2 ;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += merge(arr, low, mid, high);  // merging sorted halves
+        return cnt;
+    }
+
+    public static int numberOfInversions(int[] a, int n) {
+        // Count the number of pairs:
+        return mergeSort(a, 0, n - 1);
+    }
