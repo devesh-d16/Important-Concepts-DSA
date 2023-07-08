8a9dd09c2a63e16c36d562267de630be8d2ec492
devesh-d16 <77147378+devesh-d16@users.noreply.github.com>
2023-07-08 16:33:58 +0530

    duplicate element wala code bi hai isme

--- SearchInRotatedSortedArray.java
+++ SearchInRotatedSortedArray.java
@@ -45,3 +45,41 @@ public int search(int[] nums, int target) {
         }
         return -1;
     }
+
+// including duplicates
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        while(left <= right){
+            int mid = right - (right - left)/2;
+
+            if(nums[mid] == target){
+                return true;
+            }
+
+            if(nums[left] == nums[mid] && nums[right] == nums[mid]){
+                left = left + 1;
+                right = right - 1;
+                continue;
+            }
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+            else{
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
