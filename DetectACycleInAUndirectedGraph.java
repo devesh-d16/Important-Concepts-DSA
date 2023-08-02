a4197da778430bfc946e301a2422b2cdd8101999
devesh-d16 <77147378+devesh-d16@users.noreply.github.com>
2023-08-02 20:05:09 +0530

    bfs se kiya

---
+++ DetectACycleInAUndirectedGraph.java
@@ -0,0 +1,54 @@
+// Given an undirected graph with V vertices and E edges, check whether it contains any cycle or not. 
+// Graph is in the form of adjacency list where adj[i] contains all the nodes ith node is having edge with.
+// Expected Time Complexity: O(V + E)
+// Expected Space Complexity: O(V)
+
+// BFS
+   public class Pair{
+        int first;
+        int second;
+        
+        public Pair(int first, int second){
+            this.first = first;
+            this.second = second;
+        }
+    }
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[] = new boolean[V];
+        
+        for(int i = 0; i < V; i++){
+            if(vis[i] == false){
+                if(checkCycle(i, V, adj, vis)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean checkCycle(int i, int V, ArrayList<ArrayList<Integer>> adj, boolean vis[]){
+        
+        vis[i] = true;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i, -1));
+        
+        while(!q.isEmpty()){
+            int node = q.peek().first;
+            int parent = q.peek().second;
+            q.remove();
+            
+            for(int adjNode: adj.get(node)){
+                if(vis[adjNode] == false){
+                    vis[adjNode] = true;
+                    q.add(new Pair(adjNode, node));
+                }
+                else if(parent != adjNode){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+// DFS
