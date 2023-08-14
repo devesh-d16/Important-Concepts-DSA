0dc57a1a0bfff290f32a62d27b463e8041b825c9
devesh-d16 <77147378+devesh-d16@users.noreply.github.com>
2023-08-14 17:23:56 +0530

    disjointset question not tuff but you have to see it

---
+++ MostStonesRemovedWithSameRowOrColumn.java
@@ -0,0 +1,96 @@
+// On a 2D plane, we place n stones at some integer coordinate points. 
+// Each coordinate point may have at most one stone.
+// A stone can be removed if it shares either the same row or the same column as another stone that has not been removed.
+// Given an array stones of length n where stones[i] = [xi, yi] represents the location of the ith stone, 
+// return the largest possible number of stones that can be removed.
+
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>(); 
+    public DisjointSet(int n) {
+        for(int i = 0;i<=n;i++) {
+            rank.add(0); 
+            parent.add(i); 
+            size.add(1); 
+        }
+    }
+
+    public int findUPar(int node) {
+        if(node == parent.get(node)) {
+            return node; 
+        }
+        int ulp = findUPar(parent.get(node)); 
+        parent.set(node, ulp); 
+        return parent.get(node); 
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+        }
+        else if(rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            int rankU = rank.get(ulp_u); 
+            rank.set(ulp_u, rankU + 1); 
+        }
+    }
+    
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v); 
+            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u)); 
+        }
+        else {
+            parent.set(ulp_v, ulp_u); 
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
+class Solution {
+    public int removeStones(int[][] stones) {
+      // finding the maxRow and column of the matrix where we will traverse
+        int n = stones.length;
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for(int i = 0; i < n; i++){
+            maxRow = Math.max(maxRow, stones[i][0]);
+            maxCol = Math.max(maxCol, stones[i][1]);
+        }
+      
+
+        DisjointSet ds = new DisjointSet(maxRow + maxCol + 1);
+      // isme humlog row and columns ko as a node treat krenge
+      // starting from row and then the columns
+      // and then jo stones hai uske jo particular row and column hai unko union se connect krte jayenge
+        HashMap<Integer, Integer> stoneNodes = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            int nodeRow = stones[i][0];
+            int nodeCol = stones[i][1] + maxRow + 1; // formula to calc the column
+
+            ds.unionBySize(nodeRow, nodeCol);
+            stoneNodes.put(nodeRow, 1);
+            stoneNodes.put(nodeCol, 1);
+        }
+
+        int count = 0;
+        for(Map.Entry<Integer, Integer> it : stoneNodes.entrySet()){
+            if(ds.findUPar(it.getKey()) == it.getKey()){
+                count++;
+            }
+        }
+      // no of stones - no of components
+        return n - count;
+    }
+}
